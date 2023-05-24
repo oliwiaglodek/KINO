169bd49da42ce186d852ab6273172f6f7b5d298e
Jan Dyndor <dyndorjan@gmail.com>
2023-05-24 23:45:38 +0200

    tworzenie funkcjonalnosci stony CENNIK

--- JS.js
+++ JS.js
@@ -32,39 +32,40 @@ const nextSlide = document.querySelector(".right");
 let curSlide = 0;
 // maximum number of slides
 let maxSlide = slides.length - 1;
+if (nextSlide) {
+  // add event listener and navigation functionality
+  nextSlide.addEventListener("click", function () {
+    // check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
 
-// add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  //   move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    //   move slide by -100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
   });
-});
-
+}
 // select next slide button
 const prevSlide = document.querySelector(".left");
+if (prevSlide) {
+  // add event listener and navigation functionality
+  prevSlide.addEventListener("click", function () {
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else {
+      curSlide--;
+    }
 
-// add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
-
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
   });
-});
+}
 
 // Smooth srcolling
 const allLinks = document.querySelectorAll("a:link");
@@ -88,4 +89,25 @@ allLinks.forEach(function (link) {
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
+});
+
+// Kod od strony cennik
+const Price = document.querySelector(".ceny-bilet-link");
+const PriceGrupowe = document.querySelector(".ceny-grupowe-link");
+
+const cennik1 = document.querySelector(".cennik1");
+const cennik2 = document.querySelector(".cennik2");
+
+PriceGrupowe.addEventListener("click", function () {
+  if (cennik2.classList.contains("hidden-cennik")) {
+    cennik2.classList.remove("hidden-cennik");
+    cennik1.classList.add("hidden-cennik");
+  }
+});
+
+Price.addEventListener("click", function () {
+  if (cennik1.classList.contains("hidden-cennik")) {
+    cennik1.classList.remove("hidden-cennik");
+    cennik2.classList.add("hidden-cennik");
+  }
 });
