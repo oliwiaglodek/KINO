abde2b06b87c7c757c7b8cc0d2700cc3b3ea2ae4
OliwiaGłodek <126956455+oliwiaglodek@users.noreply.github.com>
2023-05-27 11:54:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,28 @@
-const container = document.querySelector('.container'); 
-const seats = document.querySelectorAll('.row .seat:not(.occupied'); 
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-const movieSelect = document.getElementById('movie');
 
-populateUI();//liczba wybranych miejsc
-let ticketPrice = +movieSelect.value; 
+const seats = document.querySelectorAll(".seat");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
 
-// zapsuje film i cenę
-function setMovieData(movieIndex, moviePrice) {
-    localStorage.setItem('selectedMovieIndex', movieIndex); //przechowuje wybrany film
-    localStorage.setItem('selectedMoviePrice', moviePrice); //przechowuje cene filmu
-}
+let selectedSeats = [];
+let ticketPrice = 10; // Cena biletu
 
-// update total i count
+// Aktualizacja liczby wybranych miejsc i kosztu
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selected = document.querySelectorAll(".seat.selected");
+  const selectedSeatsCount = selected.length;
 
-    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
-    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));//przechowuje tylko dane tekstowe 
-
-    const selectedSeatsCount = selectedSeats.length;//przypisuje ilość wybranych miejsc do count 
-
-    count.innerText = selectedSeatsCount;//aktualizuje count 
-    total.innerText = selectedSeatsCount * ticketPrice;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-function populateUI() {
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-    if (selectedSeats !== null && selectedSeats.length > 0) {
-        seats.forEach((seat, index) => {
-            if (selectedSeats.indexOf(index) > -1) {
-                seat.classList.add('selected'); //zmiana koloru miejsca
-            }
-        });
-    }
+// Obsługa kliknięcia na miejsce
+function seatClickHandler() {
+  this.classList.toggle("selected");
 
-    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex'); //zapamiętuje wybrane miejsca
-
-    if (selectedMovieIndex !== null) {
-        movieSelect.selectedIndex = selectedMovieIndex;
-    }
+  updateSelectedCount();
 }
 
-// wybór filmu - sprawdza czy film nie został zmieniony, wtedy aktualizuje cene i film
-movieSelect.addEventListener('change', (e) => { 
-    ticketPrice = +e.target.value;
-    setMovieData(e.target.selectedIndex, e.target.value);
-    updateSelectedCount();
+// Dodajemy nasłuchiwanie kliknięcia dla każdego miejsca
+seats.forEach((seat) => {
+  seat.addEventListener("click", seatClickHandler);
 });
-
-// wybór siedzeń - sprawdza czy miejsce nie jest zajęte i obsługuje kliknięcie
-container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
-
-        updateSelectedCount();
-    }
-});
-
-// aktulaizuje info o liczbie miejsc i ceny
-updateSelectedCount();
