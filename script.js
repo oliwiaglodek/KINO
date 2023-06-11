e0033f34d42273cd879aa63e39426f2ac780b566
Jan Dyndor <dyndorjan@gmail.com>
2023-06-11 20:39:48 +0200

    zmiana wszystkiego

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+const seats = document.querySelectorAll(".seat");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+
+let selectedSeats = [];
+let ticketPrice = 10; // Cena biletu
+
+// Aktualizacja liczby wybranych miejsc i kosztu
+function updateSelectedCount() {
+  const selected = document.querySelectorAll(".seat.selected");
+  const selectedSeatsCount = selected.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Obsługa kliknięcia na miejsce
+function seatClickHandler() {
+  this.classList.toggle("selected");
+
+  updateSelectedCount();
+}
+
+// Dodajemy nasłuchiwanie kliknięcia dla każdego miejsca
+seats.forEach((seat) => {
+  seat.addEventListener("click", seatClickHandler);
+});
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
